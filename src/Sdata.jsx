6c30812430f446ab86865f9c7d49c54bc5992b3c
Vicky Kumar Shaw <97086345+vicky-ks@users.noreply.github.com>
2023-07-06 14:10:17 +0530

    Update Sdata.jsx
    added id in array element

--- src/Sdata.jsx
+++ src/Sdata.jsx
@@ -1,23 +1,27 @@
 const Sdata = [
   {
+    id: 1,
     sname: "MISSION MAJNU",
     imgscr: "http://rb.gy/pjutx",
     title: "NETFLIX ORIGINAL SERIES",
     links: "https://www.netflix.com/in/title/81633804",
   },
   {
+    id: 2,
     sname: "KANTARA",
     imgscr: "https://rb.gy/h3wee",
     title: "NETFLIX ORIGINAL SERIES",
     links: "https://www.netflix.com/in/title/81656709",
   },
   {
+    id: 3,
     sname: "GOD FATHER",
     imgscr: "http://rb.gy/qlyq2",
     title: "NETFLIX ORIGINAL SERIES",
     links: "https://www.netflix.com/in/title/81648072",
   },
   {
+    id: 4,
     sname: "JOGI",
     imgscr: "http://rb.gy/mt29z",
     title: "NETFLIX ORIGINAL SERIES",
